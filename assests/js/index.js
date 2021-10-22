4bd801d134019fbce9423f045ea47145f49417ac
SumaiaSorna <sorna.sumaia@gmail.com>
2021-10-22 19:42:55 +0100

    rendered forecast cards in js

--- assests/js/index.js
+++ assests/js/index.js
@@ -61,9 +61,32 @@ const renderCurrentWeatherCard = function (currentData) {
   weatherCardsContainer.append(currentWeatherCard);
 };
 
+const renderForecastWeatherCards = function (forecastData) {
+  const constructForecastCard = function (each) {
+    return `<div class="card m-1 forecast-card">
+            <div class="card-body">
+            <h5 class="card-title">${each.date}</h5>
+            <p class="card-text"><img src="https://openweathermap.org/img/w/${each.iconCode}.png"/></p>
+            <p class="card-text">Temp: ${each.temperature}deg;F</p>
+            <p class="card-text">Wind: ${each.wind} MPH</p>
+            <p class="card-text">Humidity:${each.humidity}%</p>
+            </div>
+            </div>`;
+  };
+
+  const forecastCards = forecastData.map(constructForecastCard).join("");
+  const forecastCardContainer = `<div class="bg-white border">
+            <h3 class="p-3 text-center">5-Day Forecast:</h3>
+            <div class="m-3 d-flex flex-wrap justify-content-around">${forecastCards}</div>
+            </div>`;
+
+  weatherCardsContainer.append(forecastCardContainer);
+};
+
 // constructing weather card
 const renderWeatherCards = function (weatherData) {
-  console.log(weatherData);
   renderCurrentWeatherCard(weatherData.current);
+
+  renderForecastWeatherCards(weatherData.forecast);
 };
 renderWeatherCards(mockData);
