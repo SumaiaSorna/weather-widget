42ca0de044f8f2f99cd29a9758c2cdae3158790f
SumaiaSorna <sorna.sumaia@gmail.com>
2021-10-22 18:16:14 +0100

    renderCurrentCard in js

---
+++ assests/js/index.js
@@ -0,0 +1,69 @@
+const mockData = {
+  current: {
+    name: "London",
+    temperature: 123.45,
+    wind: 111.22,
+    humidity: 33,
+    uvi: 2.5,
+    date: "(3/30/2021)",
+    iconCode: "04n",
+  },
+  forecast: [
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n",
+    },
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n",
+    },
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n",
+    },
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n",
+    },
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n",
+    },
+  ],
+};
+
+const weatherCardsContainer = $("#weather-cards-container");
+
+const renderCurrentWeatherCard = function (currentData) {
+  const currentWeatherCard = `<div class="card-body bg-white border mb-2">
+        <h2 class="card-title">${currentData.name} ${currentData.date} <img src="https://openweathermap.org/img/w/${currentData.iconCode}.png"/></h2>
+        <p class="card-text">Temp: ${currentData.temperature}&deg;F</p>
+        <p class="card-text">Wind: ${currentData.wind} MPH</p>
+        <p class="card-text">Humidity: ${currentData.humidity}%</p>
+        <p class="card-text">UV index: <span class="btn btn-primary">${currentData.uvi}</span></p>
+        </div>`;
+
+  weatherCardsContainer.append(currentWeatherCard);
+};
+
+// constructing weather card
+const renderWeatherCards = function (weatherData) {
+  console.log(weatherData);
+  renderCurrentWeatherCard(weatherData.current);
+};
+renderWeatherCards(mockData);
